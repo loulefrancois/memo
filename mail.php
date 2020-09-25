4b51e2a3487fee1d274c8668de444ac8006904e5
Lou Lefrançois <lefrancois.lou@gmail.com>
2020-09-25 12:44:42 +0200

    test

---
+++ mail.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Envoi d'un message par formulaire</title>
+</head>
+
+<body>
+    <?php
+    $retour = mail('lefrancois.lou@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From : webmaster@monsite.fr');
+    if ($retour) {
+        echo '<p>Votre message a bien été envoyé.</p>';
+    }
+    ?>
+</body>
+
+</html>
